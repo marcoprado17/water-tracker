0bfca818069a1b7831e6fb0288674454a0b55a21
Marco Aurélio Prado <marco.pdsv@gmail.com>
2018-11-04 14:41:33 -0200

    Adding Simple app framework

---
+++ app/src/main/java/mprado/me/watertracker/HomeFragment.java
@@ -0,0 +1,15 @@
+package mprado.me.watertracker;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home, container, false);
+    }
+}
---
+++ app/src/main/java/mprado/me/watertracker/MainActivity.java
@@ -0,0 +1,56 @@
+package mprado.me.watertracker;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Fragment homeFragment = new HomeFragment();
+    private Fragment dashboardsFragment = new DashboardsFragment();
+    private Fragment notificationsFragment = new NotificationsFragment();
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_holder, homeFragment)
+                            .commit();
+                    return true;
+                case R.id.navigation_dashboards:
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_holder, dashboardsFragment)
+                            .commit();
+                    return true;
+                case R.id.navigation_notifications:
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_holder, notificationsFragment)
+                            .commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_holder, homeFragment)
+                .commit();
+    }
+}
